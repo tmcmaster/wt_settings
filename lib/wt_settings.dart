4a22c61eb0c7ee19cb4d51dd5d52e925ce896df6
Tim McMaster <tim@mcmaster.id.au>
2022-10-30 23:46:00 +1100

    added exports to the library file.

--- lib/wt_settings.dart
+++ lib/wt_settings.dart
@@ -1,7 +1,27 @@
 library wt_settings;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/base_settings_providers.dart';
+export 'src/local_storage_state_notifier.dart';
+export 'src/object_state_notifier.dart';
+export 'src/settings.dart';
+export 'src/settings_bool/settings_bool_componnet.dart';
+export 'src/settings_bool/settings_bool_notifier.dart';
+export 'src/settings_bool/settings_bool_providers.dart';
+export 'src/settings_color/settings_color_component.dart';
+export 'src/settings_color/settings_color_notifier.dart';
+export 'src/settings_color/settings_color_providers.dart';
+export 'src/settings_color/widgets/color_picker.dart';
+export 'src/settings_date/settings_date_component.dart';
+export 'src/settings_date/settings_date_notifier.dart';
+export 'src/settings_date/settings_date_providers.dart';
+export 'src/settings_enum/settings_enum_component.dart';
+export 'src/settings_enum/settings_enum_notifier.dart';
+export 'src/settings_enum/settings_enum_providers.dart';
+export 'src/settings_object/settings_object_componnet.dart';
+export 'src/settings_object/settings_object_notifier.dart';
+export 'src/settings_object/settings_object_providers.dart';
+export 'src/settings_string/settings_string_componnet.dart';
+export 'src/settings_string/settings_string_notifier.dart';
+export 'src/settings_string/settings_string_providers.dart';
+export 'src/state_notifier_listener.dart';
+export 'src/utils/logging.dart';
