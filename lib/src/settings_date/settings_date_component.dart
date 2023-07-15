dc580d8f2d452c5b31cf02a59c08e52469cc03a3
Tim McMaster <tim@mcmaster.id.au>
2023-07-16 01:56:26 +1000

    removed dependency on flutter_hooks

--- lib/src/settings_date/settings_date_component.dart
+++ lib/src/settings_date/settings_date_component.dart
@@ -1,13 +1,10 @@
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wt_logging/wt_logging.dart';
 import 'package:wt_settings/src/settings_date/settings_date_providers.dart';
 
-class SettingsDateComponent extends ConsumerWidget {
-  static final log = logger(SettingsDateComponent);
-
+class SettingsDateComponent extends ConsumerStatefulWidget {
   final SettingsDateProviders providers;
   final String label;
   final String hint;
@@ -20,10 +17,32 @@ class SettingsDateComponent extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final controller = useTextEditingController();
-    controller.text = ref.watch(providers.value);
-    final notifier = ref.read(providers.notifier);
+  ConsumerState<SettingsDateComponent> createState() => _SettingsDateComponentState();
+}
+
+class _SettingsDateComponentState extends ConsumerState<SettingsDateComponent> {
+  static final log = logger(SettingsDateComponent);
+
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+    log.d('TextEditingController has been created');
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    log.d('TextEditingController has been disposed');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    controller.text = ref.watch(widget.providers.value);
+    final notifier = ref.read(widget.providers.notifier);
 
     return SizedBox(
       width: 150,
@@ -41,3 +60,40 @@ class SettingsDateComponent extends ConsumerWidget {
     );
   }
 }
+//
+// class SettingsDateComponent extends StatelessWidget {
+//   static final log = logger(SettingsDateComponent);
+//
+//   final SettingsDateProviders providers;
+//   final String label;
+//   final String hint;
+//
+//   const SettingsDateComponent({
+//     super.key,
+//     required this.providers,
+//     required this.label,
+//     required this.hint,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final controller = useTextEditingController();
+//     controller.text = ref.watch(providers.value);
+//     final notifier = ref.read(providers.notifier);
+//
+//     return SizedBox(
+//       width: 150,
+//       child: DateTimePicker(
+//         type: DateTimePickerType.date,
+//         controller: controller,
+//         firstDate: DateTime(2020),
+//         lastDate: DateTime(2024),
+//         icon: const Icon(Icons.event),
+//         dateLabelText: 'From',
+//         onChanged: (value) {
+//           notifier.replaceValue(value);
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/src/settings_string/settings_string_componnet.dart
+++ lib/src/settings_string/settings_string_componnet.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wt_logging/wt_logging.dart';
 import 'package:wt_settings/src/settings_string/settings_string_providers.dart';
 
-class SettingsStringComponent extends ConsumerWidget {
-  static final log = logger(SettingsStringComponent);
-
+class SettingsStringComponent extends ConsumerStatefulWidget {
   final SettingsStringProviders providers;
   final String label;
   final String hint;
@@ -19,12 +16,33 @@ class SettingsStringComponent extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final focusNode = useFocusNode();
-    final tokenEdit = useTextEditingController();
+  ConsumerState<SettingsStringComponent> createState() => _SettingsStringComponentState();
+}
 
-    tokenEdit.text = ref.watch(providers.value);
-    final notifier = ref.read(providers.notifier);
+class _SettingsStringComponentState extends ConsumerState<SettingsStringComponent> {
+  static final log = logger(SettingsStringComponent);
+
+  late FocusNode focusNode;
+  late TextEditingController tokenEdit;
+
+  @override
+  void initState() {
+    focusNode = FocusNode();
+    tokenEdit = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    tokenEdit.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    tokenEdit.text = ref.watch(widget.providers.value);
+    final notifier = ref.read(widget.providers.notifier);
 
     focusNode.addListener(() {
       log.d('-- Value has changed: ${tokenEdit.text}');
@@ -39,10 +57,54 @@ class SettingsStringComponent extends ConsumerWidget {
         notifier.replaceValue(tokenEdit.text);
       },
       decoration: InputDecoration(
-        labelText: label,
-        hintText: hint,
+        labelText: widget.label,
+        hintText: widget.hint,
         prefixIcon: const Icon(Icons.key),
       ),
     );
   }
 }
+//
+//
+// class SettingsStringComponent extends ConsumerWidget {
+//   static final log = logger(SettingsStringComponent);
+//
+//   final SettingsStringProviders providers;
+//   final String label;
+//   final String hint;
+//
+//   const SettingsStringComponent({
+//     super.key,
+//     required this.providers,
+//     required this.label,
+//     required this.hint,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final focusNode = useFocusNode();
+//     final tokenEdit = useTextEditingController();
+//
+//     tokenEdit.text = ref.watch(providers.value);
+//     final notifier = ref.read(providers.notifier);
+//
+//     focusNode.addListener(() {
+//       log.d('-- Value has changed: ${tokenEdit.text}');
+//       notifier.replaceValue(tokenEdit.text);
+//     });
+//
+//     return TextField(
+//       controller: tokenEdit,
+//       focusNode: focusNode,
+//       onEditingComplete: () {
+//         log.d('onEditingComplete. saving value: ${tokenEdit.text}');
+//         notifier.replaceValue(tokenEdit.text);
+//       },
+//       decoration: InputDecoration(
+//         labelText: label,
+//         hintText: hint,
+//         prefixIcon: const Icon(Icons.key),
+//       ),
+//     );
+//   }
+// }
